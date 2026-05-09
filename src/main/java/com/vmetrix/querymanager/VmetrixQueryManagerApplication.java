795e5cb1f8afd12031ed285dabf7849d3dca9c55
jparaos <jpj.araos@gmail.com>
2026-05-09 18:44:19 -0400

    Spring boot project initialization

---
+++ src/main/java/com/vmetrix/querymanager/VmetrixQueryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vmetrix.querymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmetrixQueryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmetrixQueryManagerApplication.class, args);
+    }
+
+}
